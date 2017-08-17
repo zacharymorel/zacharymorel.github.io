2b59bfd67ce6e170f2ea2a164ef34f74666705a8
Zachary Morel <morelz133@gmail.com>
2017-08-17 09:31:13 -0400

    Safari mobile not wokring well for responsive rendering

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,109 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
+var header = require('gulp-header')
+var cleanCSS = require('gulp-clean-css')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+var pkg = require('./package.json')
 
 // Set the banner content
-var banner = ['/*!\n',
+var banner = [
+  '/*!\n',
   ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Copyright 2013-' + new Date().getFullYear(),
+  ' <%= pkg.author %>\n',
   ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
   ' */\n',
   ''
-].join('');
+].join('')
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/creative.scss')
+  return gulp
+    .src('scss/creative.scss')
     .pipe(sass())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
+    .pipe(
+      header(banner, {
+        pkg: pkg
+      })
+    )
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    )
+})
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/creative.css')
-    .pipe(cleanCSS({
-      compatibility: 'ie8'
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
+  return gulp
+    .src('css/creative.css')
+    .pipe(
+      cleanCSS({
+        compatibility: 'ie8'
+      })
+    )
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    )
+})
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/creative.js')
+  return gulp
+    .src('js/creative.js')
     .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      header(banner, {
+        pkg: pkg
+      })
+    )
+    .pipe(
+      rename({
+        suffix: '.min'
+      })
+    )
     .pipe(gulp.dest('js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    )
+})
 
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
+  gulp
+    .src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
     .pipe(gulp.dest('vendor/bootstrap'))
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+  gulp
+    .src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
     .pipe(gulp.dest('vendor/jquery'))
 
-  gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'))
+  gulp.src(['node_modules/magnific-popup/dist/*']).pipe(gulp.dest('vendor/magnific-popup'))
 
-  gulp.src(['node_modules/scrollreveal/dist/*.js'])
-    .pipe(gulp.dest('vendor/scrollreveal'))
+  gulp.src(['node_modules/scrollreveal/dist/*.js']).pipe(gulp.dest('vendor/scrollreveal'))
 
-  gulp.src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+  gulp
+    .src(['node_modules/popper.js/dist/umd/popper.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(gulp.dest('vendor/popper'))
 
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
+  gulp.src(['node_modules/jquery.easing/*.js']).pipe(gulp.dest('vendor/jquery-easing'))
 
-  gulp.src([
+  gulp
+    .src([
       'node_modules/font-awesome/**',
       '!node_modules/font-awesome/**/*.map',
       '!node_modules/font-awesome/.npmignore',
@@ -98,23 +115,24 @@ gulp.task('copy', function() {
 })
 
 // Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy'])
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: './'
     },
+    startPath: '/html'
   })
 })
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['minify-css']);
-  gulp.watch('js/*.js', ['minify-js']);
+  gulp.watch('scss/*.scss', ['sass'])
+  gulp.watch('css/*.css', ['minify-css'])
+  gulp.watch('js/*.js', ['minify-js'])
   // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
-});
+  gulp.watch('*.html', browserSync.reload)
+  gulp.watch('js/**/*.js', browserSync.reload)
+})
