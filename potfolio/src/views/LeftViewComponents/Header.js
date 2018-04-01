7796dcc38d1313b26a3945a617c7a432d559c35a
Zachary Morel <morelz133@gmail.com>
2018-03-31 21:45:20 -0400

    More Css Mobile Work.

---
+++ potfolio/src/views/LeftViewComponents/Header.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import NavigationButton from '../../utils/NavigationButton'
+
+import '../../css/leftView.css'
+import '../../css/mobile/leftViewMobile.css'
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeButton: 'ProjectDiscription',
+      showContactHeader: false,
+    }
+  }
+
+  toggleComponents = (params) => {
+    if(params === 'ProjectDiscription') {
+      if(this.props.determineContent !== params ) {
+        this.setState({ activeButton: params, showContactHeader: false }) 
+        this.props.headerDetermineContentCallBack(params)
+      }
+    }
+
+    if(params === 'AboutMe') {
+      if(this.props.determineContent !== params ) {
+        this.setState({ activeButton: params, showContactHeader: false }) 
+        this.props.headerDetermineContentCallBack(params)
+      }
+    }
+
+    if(params === 'Contact')
+      !this.state.showContactHeader ? 
+        this.setState({ activeButton: params, showContactHeader: true }) : 
+        this.setState({ showContactHeader: false })
+  }
+
+  render() {
+    return (
+      <header className='Header'>
+        <h1 className='HeaderTitle'>Zachary Morel.</h1>
+        <div className='Navigation'>
+          <NavigationButton 
+            buttonClassName='HeaderButtons' 
+            onClick={() => this.toggleComponents('ProjectDiscription') } 
+            styles={{ borderBottom: (this.state.activeButton === 'ProjectDiscription' && '1px solid #373940')}}
+          >
+            <p className='Text'>Home</p>
+          </NavigationButton>
+          <NavigationButton 
+            buttonClassName='HeaderButtons' 
+            onClick={() => this.toggleComponents('AboutMe') }
+            styles={{ borderBottom: (this.state.activeButton === 'AboutMe' && '1px solid #373940')}}
+          >
+            <p className='Text'>About</p>
+          </NavigationButton>
+          <NavigationButton
+            value='Contact'
+            buttonClassName='HeaderButtons' 
+            onClick={() => this.toggleComponents('Contact')} 
+            styles={{ borderBottom: (this.state.activeButton === 'Contact' && '1px solid #373940')}}
+          >
+            <p className='Text'>Contact</p>
+          </NavigationButton>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header 
--- potfolio/src/views/LeftViewComponents/LeftView.js
+++ potfolio/src/views/LeftViewComponents/LeftView.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 
 // Components
 import ProjectDiscription from './ProjectDiscription'
-import NavigationButton from '../../utils/NavigationButton'
+import Header from './Header'
 import AboutMe from './AboutMe'
 import Contact from './Contact'
 import { PortfolioConfig } from '../../PortfolioConfig'
@@ -19,8 +19,6 @@ class LeftView extends PureComponent {
       projectName: '', 
       projectDiscriptionText: '',
       determineContent: 'ProjectDiscription',
-      activeButton: 'ProjectDiscription',
-      showContactHeader: false,
     }
   }
 
@@ -38,49 +36,14 @@ class LeftView extends PureComponent {
     }
   }
 
-  toggleComponents = (params) => {
-    if(params === 'ProjectDiscription')
-      this.state.determineContent !== params && this.setState({ activeButton: params, determineContent: params, showContactHeader: false }) 
-
-    if(params === 'AboutMe')
-      this.state.determineContent !== params && this.setState({ activeButton: params, determineContent: params, showContactHeader: false }) 
-
-    if(params === 'Contact')
-      !this.state.showContactHeader ? 
-        this.setState({ activeButton: params, showContactHeader: true }) : 
-        this.setState({ showContactHeader: false })
+  headerDetermineContentCallBack = (params) => {
+    this.setState({ determineContent: params })
   }
 
   render() {
     return (
       <div className='LeftViewParent'>
-        <header className='LeftHeader'>
-          <h1 className='LeftTitle'>Zachary Morel.</h1>
-          <div className='Navigation'>
-            <NavigationButton 
-              buttonClassName='HeaderButtons' 
-              onClick={() => this.toggleComponents('ProjectDiscription') } 
-              styles={{ borderBottom: (this.state.activeButton === 'ProjectDiscription' && '1px solid #373940')}}
-            >
-              <p className='Text'>Home</p>
-            </NavigationButton>
-            <NavigationButton 
-              buttonClassName='HeaderButtons' 
-              onClick={() => this.toggleComponents('AboutMe') }
-              styles={{ borderBottom: (this.state.activeButton === 'AboutMe' && '1px solid #373940')}}
-            >
-              <p className='Text'>About</p>
-            </NavigationButton>
-            <NavigationButton
-              value='Contact'
-              buttonClassName='HeaderButtons' 
-              onClick={() => this.toggleComponents('Contact')} 
-              styles={{ borderBottom: (this.state.activeButton === 'Contact' && '1px solid #373940')}}
-            >
-              <p className='Text'>Contact</p>
-            </NavigationButton>
-          </div>
-        </header>
+        <Header headerDetermineContentCallBack={this.headerDetermineContentCallBack} />
         {this.state.showContactHeader && <Contact />}
         {this.state.determineContent === 'ProjectDiscription' &&  
           <ProjectDiscription 
