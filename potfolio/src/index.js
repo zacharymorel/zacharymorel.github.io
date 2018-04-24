4174d0c5ed4ca79ac965e758c3da7af8a402b1d5
Zachary Morel <morelz133@gmail.com>
2018-04-23 20:09:33 -0400

    CHanging Styles and files again

--- potfolio/src/index.js
+++ potfolio/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/theme.css';
+import './ThemeCss/theme.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
